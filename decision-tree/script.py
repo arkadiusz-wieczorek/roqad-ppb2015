cf919fce0c5b3c9b3681f955caccc3c37198c6ee
debian-sh <bestmatrix@gmail.com>
2015-09-03 11:53:12 +0200

    add once loading file with problems

--- decision-tree/script.py
+++ decision-tree/script.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 
 import Orange
-import numpy
 data = Orange.data.Table("sample_data.tab")
-data2 = Orange.data.Table("2_sample_data.tab")
+# data2 = Orange.data.Table("sample_data.tab")
+# data2 = data.select(data.domain)
 
+data2 = Orange.data.Table(data)
+data2.domain = Orange.data.Domain(data.domain)
+
+
+for inst in data2.domain:
+	data2.domain[inst].name = '__' + data2.domain[inst].name
+
+print data.domain
+print data2.domain
 
 table = data.select(['user_id'])
-table2 = data2.select(['2_user_id'])
+table2 = data2.select(['__user_id'])
 
+new_table = Orange.data.Table([data, data2])
+print len(new_table)
 
-# for inst in table:
-#     selected_user_id = inst[0].native()
-#     for inst2 in table2:
-#     	if selected_user_id == inst2[0].native():
-#     		print('TAK')
-#     	else:
-#     		print('NIE')
+new_cleaned_table = Orange.data.Table(new_table.domain)
+print new_cleaned_table.domain
 
+predict_list = []
 
-# predict_table = Orange.data.Domain[data.domain,data2.domain]
-# predict_table = numpy.concatenate([data.domain, data2.domain])
-# predict_table = Orange.data.Domain([data.domain, data2.domain])	
-# print predict_table
-# new_domain = Orange.data.Domain(printed_data_domain, data2.domain)
-
-new_table = Orange.data.Table(data.domain, [])
-
-new_table_2 = Orange.data.Table(data2.domain, new_table)
-print new_table_2.domain
+for inst in table:
+    selected_user_id = inst[0].native()
+    for inst2 in table2:
+    	if selected_user_id == inst2[0].native():
+    		predict_list.append('TAK')
+    	else:
+    		predict_list.append('NIE')
