5b349fefb969c58b05319a2b57e540a8d20dd65a
debian-sh <bestmatrix@gmail.com>
2015-09-09 11:27:45 +0200

    Add script for getting sample from dataset

---
+++ python/random_sample_from_requests.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/env python
+import fileinput
+import random
+
+def getSample(dataset_file, percent_sample):
+	f=open(dataset_file)
+	dataset=f.readlines()
+
+	array_length = len(dataset) - 1 #without domain row
+	number_of_samples = int(array_length*percent_sample)
+	sample = ""
+	samples = dataset[0]
+	
+	for x in range(number_of_samples):
+		sample = random.choice(dataset)
+		samples += sample
+	pass
+	return samples
+
+print getSample('../roq-ad-data-set/learning-set/requests.csv', 0.15)
