eec70263714f39bb0fc16a2e27f8414232a01013
debian-sh <bestmatrix@gmail.com>
2015-09-09 11:52:22 +0200

    Change return type (from string to array)

--- python/random_sample_from_dataset.py
+++ python/random_sample_from_dataset.py
@@ -9,10 +9,11 @@ def getSample(dataset_file, percent_sample):
 	array_length = len(dataset) - 1 #without domain row
 	number_of_samples = int(array_length*percent_sample)
 	sample = ""
-	samples = dataset[0]
-	
+	samples = []
+	samples.append(dataset[0])
+
 	for x in range(number_of_samples):
 		sample = random.choice(dataset)
-		samples += sample
+		samples.append(sample)
 	pass
 	return samples
