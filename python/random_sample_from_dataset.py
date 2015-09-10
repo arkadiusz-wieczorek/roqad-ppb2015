0bf652d60bc99b87e8e1697d6f72c366c60dee4a
debian-sh <bestmatrix@gmail.com>
2015-09-10 11:32:37 +0200

    Fix for using arrays

--- python/random_sample_from_dataset.py
+++ python/random_sample_from_dataset.py
@@ -2,9 +2,12 @@
 import fileinput
 import random
 
-def getSample(dataset_file, percent_sample):
-	f=open(dataset_file)
-	dataset=f.readlines()
+def getSample(dataset_file, percent_sample, mode):
+	if mode == 1:
+		f=open(dataset_file)
+		dataset = f.readlines()
+	else:
+		dataset = dataset_file
 
 	array_length = len(dataset) - 1 #without domain row
 	number_of_samples = int(array_length*percent_sample)
