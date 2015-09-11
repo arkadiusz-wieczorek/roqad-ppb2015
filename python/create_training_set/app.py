32fec172319999bf2abd1651f20136118668d376
debian-sh <bestmatrix@gmail.com>
2015-09-11 11:16:25 +0200

    Add cartesianProduct function

--- python/create_training_set/app.py
+++ python/create_training_set/app.py
@@ -6,6 +6,7 @@ import reader
 
 samplecreator = imp.load_source('random_sample_from_dataset','../random_sample_from_dataset.py')
 add_user_id = imp.load_source('add_user_id_to_choosen_dataset', '../add_user_id_to_choosen_dataset.py')
+cartesian_product = imp.load_source('cartesian_product', '../cartesian_product.py')
 
 
 # sample = add_user_id.addUserID('../../roq-ad-data-set/learning-set/requests.csv','../../roq-ad-data-set/learning-set/labels.csv')
@@ -14,14 +15,14 @@ add_user_id = imp.load_source('add_user_id_to_choosen_dataset', '../add_user_id_
 sample = samplecreator.getSample('../../roq-ad-data-set/learning-set/requests.csv', 0.02, 1)
 sample = add_user_id.addUserID(sample, '../../roq-ad-data-set/learning-set/labels.csv', 0)
 
-reader = reader.Reader()
-data = reader.readFromTable(sample)
+cartesian_product.cartesianProduct(sample)
+
+# reader = reader.Reader()
+# data = reader.readFromTable(sample)
 
 
-
-training_set_creator = create_training_set.Training_set_creator(data['names'], data['data'])
-print training_set_creator.training_data
-print 'end'
+# training_set_creator = create_training_set.Training_set_creator(data['names'], data['data'])
+# print training_set_creator.training_data
 #print training_set_creator.data
 #print training_set_creator.values_maps['device_id'].value_to_number
 #print training_set_creator.values_maps['device_id'].number_to_value
